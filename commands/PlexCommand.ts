7caa40f5faa0c64e54c2f38282dad50d1498adb8
Thomas Gardner <thomasgardner@orthofix.com>
2019-05-23 15:37:36 -0500

    Cleaned up servers list display, and merged set-servers into login

--- commands/PlexCommand.ts
+++ commands/PlexCommand.ts
@@ -5,13 +5,13 @@ import { sendAttachmentNotification } from '../lib/helpers/sendAttachmentNotific
 import { sendMediaMetadata } from '../lib/helpers/sendMediaMetadata';
 import { sendNotification } from '../lib/helpers/sendNotification';
 import { sendServerDetailsNotification } from '../lib/helpers/sendServerDetailsNotification';
+import { sendServersDetailsNotification } from '../lib/helpers/sendServersDetailsNotification';
 import { sendTokenExpired } from '../lib/helpers/sendTokenExpired';
 import { AppPersistence } from '../lib/persistence';
 import { PlexApp } from '../PlexApp';
 
 enum Command {
   Login = 'login',
-  SetServers = 'set-servers',
   Servers = 'servers',
   Server = 'server',
   Search = 'search',
@@ -31,9 +31,6 @@ export class PlexCommand implements ISlashCommand {
     switch (command) {
       case Command.Login:
         await this.processLoginCommand(context, read, modify, http, persis);
-        break;
-      case Command.SetServers:
-        await this.processSetServersCommand(context, read, modify, http, persis);
         break;
       case Command.Servers:
         await this.processServersCommand(context, read, modify, http, persis);
@@ -55,11 +52,11 @@ export class PlexCommand implements ISlashCommand {
       return;
     }
 
-    const url = 'https://plex.tv/users/sign_in.json?user[login]=' + username + '&user[password]=' + password;
+    let url = 'https://plex.tv/users/sign_in.json?user[login]=' + username + '&user[password]=' + password;
 
-    const loginResponse = await http.post(url, {
-      headers: defaultHeaders,
-    });
+    const headers = defaultHeaders;
+
+    const loginResponse = await http.post(url, {headers});
 
     let token;
     if (loginResponse && loginResponse.statusCode === 201 && loginResponse.content) {
@@ -75,16 +72,142 @@ export class PlexCommand implements ISlashCommand {
         await persistence.setUserId(userId, context.getSender());
         await persistence.setUserUuid(userUuid, context.getSender());
         await persistence.setUserThumb(userThumbUrl, context.getSender());
-        await sendAttachmentNotification({
-          collapsed: true,
-          color: '#00CE00',
-          thumbnailUrl: userThumbUrl,
-          title: {
-            value: 'Logged in!',
-            link: 'https://app.plex.tv/desktop#!/account',
-          },
-          text: '*Id: *' + userId + '\n*Uuid: *' + userUuid + '\n*Token: *' + token,
-        }, read, modify, context.getSender(), context.getRoom());
+        // Now handle setting servers
+        url = 'https://plex.tv/pms/servers';
+        headers['X-Plex-Token'] = token;
+        const response = await http.get(url, {headers});
+        if (response) {
+          if (response.statusCode === 401) {
+            await sendTokenExpired(read, modify, context.getSender(), context.getRoom(), persis);
+          } else if (response.statusCode === 200 && response.content) {
+            try {
+              const serverContent = response.content;
+              const regex = new RegExp('(?<=<Server )(.*?)(?=\/>)', 'gm');
+
+              const servers = new Array<string>();
+              let m;
+              // tslint:disable-next-line:no-conditional-assignment
+              while ((m = regex.exec(serverContent)) !== null) {
+                // This is necessary to avoid infinite loops with zero-width matches
+                if (m.index === regex.lastIndex) {
+                  regex.lastIndex++;
+                }
+                // The result can be accessed through the `m`-variable.
+                m.forEach((match, groupIndex) => {
+                  if (groupIndex === 1) {
+                    servers.push(match);
+                  }
+                });
+              }
+
+              const serversObj = new Array<object>();
+              servers.forEach(async (server) => {
+                const accessTokenRegex = new RegExp('accessToken="(.*?)"', 'gm');
+                const accessTokenResult = accessTokenRegex.exec(server);
+                let accessToken;
+                if (accessTokenResult && accessTokenResult.length >= 2) {
+                  accessToken = accessTokenResult[1];
+                }
+
+                const nameRegex = new RegExp('name="(.*?)"', 'gm');
+                const nameResult = nameRegex.exec(server);
+                let name;
+                if (nameResult && nameResult.length >= 2) {
+                  name = nameResult[1];
+                }
+
+                const addressRegex = new RegExp('address="(.*?)"', 'gm');
+                const addressResult = addressRegex.exec(server);
+                let address;
+                if (addressResult && addressResult.length >= 2) {
+                  address = addressResult[1];
+                }
+
+                const portRegex = new RegExp('port="(.*?)"', 'gm');
+                const portResult = portRegex.exec(server);
+                let port;
+                if (portResult && portResult.length >= 2) {
+                  port = portResult[1];
+                }
+
+                const versionRegex = new RegExp('version="(.*?)"', 'gm');
+                const versionResult = versionRegex.exec(server);
+                let version;
+                if (versionResult && versionResult.length >= 2) {
+                  version = versionResult[1];
+                }
+
+                const schemeRegex = new RegExp('scheme="(.*?)"', 'gm');
+                const schemeResult = schemeRegex.exec(server);
+                let scheme;
+                if (schemeResult && schemeResult.length >= 2) {
+                  scheme = schemeResult[1];
+                }
+
+                const sourceTitleRegex = new RegExp('sourceTitle="(.*?)"', 'gm');
+                const sourceTitleResult = sourceTitleRegex.exec(server);
+                let sourceTitle;
+                if (sourceTitleResult && sourceTitleResult.length >= 2) {
+                  sourceTitle = sourceTitleResult[1];
+                }
+
+                const ownerIdRegex = new RegExp('ownerId="(.*?)"', 'gm');
+                const ownerIdResult = ownerIdRegex.exec(server);
+                let ownerId;
+                if (ownerIdResult && ownerIdResult.length >= 2) {
+                  ownerId = ownerIdResult[1];
+                }
+
+                const ownedRegex = new RegExp('owned="(.*?)"', 'gm');
+                const ownedResult = ownedRegex.exec(server);
+                let owned = false;
+                if (ownedResult && ownedResult.length >= 2) {
+                  if (ownedResult[1] === '1') {
+                    owned = true;
+                  }
+                }
+
+                const machineIdRegex = new RegExp('machineIdentifier="(.*?)"', 'gm');
+                const machineIdResult = machineIdRegex.exec(server);
+                let machineId;
+                if (machineIdResult && machineIdResult.length >= 2) {
+                  machineId = machineIdResult[1];
+                }
+
+                const serverObj = {
+                  accessToken, name, address, port, version, scheme, sourceTitle, ownerId, owned, machineId,
+                };
+
+                serversObj.push(serverObj);
+              });
+              const jsonServersObj = JSON.stringify(serversObj);
+
+              await persistence.setServers(jsonServersObj, context.getSender());
+
+              await sendAttachmentNotification({
+                collapsed: false,
+                color: '#00CE00',
+                thumbnailUrl: userThumbUrl,
+                title: {
+                  value: 'Logged in!',
+                  link: 'https://app.plex.tv/desktop#!/account',
+                },
+                text: '*Id: *' + userId + '\n*Uuid: *' + userUuid + '\n*Token: *' + token + '\nTo see servers, run `/plex servers`',
+              }, read, modify, context.getSender(), context.getRoom());
+            } catch (e) {
+              console.error('Failed to parse servers!', e);
+              await sendAttachmentNotification({
+                collapsed: false,
+                color: '#e10000',
+                title: {
+                  value: 'Failed to set servers!',
+                  link: 'https://app.plex.tv/desktop#!/account',
+                },
+                text: 'Logged in, but failed to parse server call from API.',
+              }, read, modify, context.getSender(), context.getRoom());
+            }
+          }
+        }
       } else {
         await sendAttachmentNotification({
           collapsed: false,
@@ -109,153 +232,6 @@ export class PlexCommand implements ISlashCommand {
     }
   }
 
-  private async processSetServersCommand(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp, persis: IPersistence): Promise<void> {
-    const persistence = new AppPersistence(persis, read.getPersistenceReader());
-    const token = await persistence.getUserToken(context.getSender());
-    if (!token) {
-      // tslint:disable-next-line:max-line-length
-      await sendNotification('No token detected! Please login first using `/plex login [USERNAME] [PASSWORD]`', read, modify, context.getSender(), context.getRoom());
-    } else {
-      const url = 'https://plex.tv/pms/servers';
-      const headers = defaultHeaders;
-      headers['X-Plex-Token'] = token;
-      const response = await http.get(url, {headers});
-      if (response) {
-        if (response.statusCode === 401) {
-          await sendTokenExpired(read, modify, context.getSender(), context.getRoom(), persis);
-        } else if (response.statusCode === 200 && response.content) {
-          const userThumbUrl = await persistence.getUserThumb(context.getSender());
-          try {
-            const content = response.content;
-            const regex = new RegExp('(?<=<Server )(.*?)(?=\/>)', 'gm');
-
-            const servers = new Array<string>();
-            let m;
-            // tslint:disable-next-line:no-conditional-assignment
-            while ((m = regex.exec(content)) !== null) {
-              // This is necessary to avoid infinite loops with zero-width matches
-              if (m.index === regex.lastIndex) {
-                regex.lastIndex++;
-              }
-              // The result can be accessed through the `m`-variable.
-              m.forEach((match, groupIndex) => {
-                if (groupIndex === 1) {
-                  servers.push(match);
-                }
-              });
-            }
-
-            const serversObj = new Array<object>();
-            servers.forEach(async (server) => {
-              const accessTokenRegex = new RegExp('accessToken="(.*?)"', 'gm');
-              const accessTokenResult = accessTokenRegex.exec(server);
-              let accessToken;
-              if (accessTokenResult && accessTokenResult.length >= 2) {
-                accessToken = accessTokenResult[1];
-              }
-
-              const nameRegex = new RegExp('name="(.*?)"', 'gm');
-              const nameResult = nameRegex.exec(server);
-              let name;
-              if (nameResult && nameResult.length >= 2) {
-                name = nameResult[1];
-              }
-
-              const addressRegex = new RegExp('address="(.*?)"', 'gm');
-              const addressResult = addressRegex.exec(server);
-              let address;
-              if (addressResult && addressResult.length >= 2) {
-                address = addressResult[1];
-              }
-
-              const portRegex = new RegExp('port="(.*?)"', 'gm');
-              const portResult = portRegex.exec(server);
-              let port;
-              if (portResult && portResult.length >= 2) {
-                port = portResult[1];
-              }
-
-              const versionRegex = new RegExp('version="(.*?)"', 'gm');
-              const versionResult = versionRegex.exec(server);
-              let version;
-              if (versionResult && versionResult.length >= 2) {
-                version = versionResult[1];
-              }
-
-              const schemeRegex = new RegExp('scheme="(.*?)"', 'gm');
-              const schemeResult = schemeRegex.exec(server);
-              let scheme;
-              if (schemeResult && schemeResult.length >= 2) {
-                scheme = schemeResult[1];
-              }
-
-              const sourceTitleRegex = new RegExp('sourceTitle="(.*?)"', 'gm');
-              const sourceTitleResult = sourceTitleRegex.exec(server);
-              let sourceTitle;
-              if (sourceTitleResult && sourceTitleResult.length >= 2) {
-                sourceTitle = sourceTitleResult[1];
-              }
-
-              const ownerIdRegex = new RegExp('ownerId="(.*?)"', 'gm');
-              const ownerIdResult = ownerIdRegex.exec(server);
-              let ownerId;
-              if (ownerIdResult && ownerIdResult.length >= 2) {
-                ownerId = ownerIdResult[1];
-              }
-
-              const ownedRegex = new RegExp('owned="(.*?)"', 'gm');
-              const ownedResult = ownedRegex.exec(server);
-              let owned = false;
-              if (ownedResult && ownedResult.length >= 2) {
-                if (ownedResult[1] === '1') {
-                  owned = true;
-                }
-              }
-
-              const machineIdRegex = new RegExp('machineIdentifier="(.*?)"', 'gm');
-              const machineIdResult = machineIdRegex.exec(server);
-              let machineId;
-              if (machineIdResult && machineIdResult.length >= 2) {
-                machineId = machineIdResult[1];
-              }
-
-              const serverObj = {
-                accessToken, name, address, port, version, scheme, sourceTitle, ownerId, owned, machineId,
-              };
-
-              serversObj.push(serverObj);
-            });
-            const jsonServersObj = JSON.stringify(serversObj);
-
-            await persistence.setServers(jsonServersObj, context.getSender());
-
-            await sendAttachmentNotification({
-              collapsed: true,
-              color: '#00CE00',
-              thumbnailUrl: userThumbUrl,
-              title: {
-                value: 'Successfully Set Servers!',
-                link: 'https://app.plex.tv/desktop#!/account',
-              },
-              text: 'To see servers, run `/plex servers`',
-            }, read, modify, context.getSender(), context.getRoom());
-          } catch (e) {
-            console.error('Failed to parse servers!', e);
-            await sendAttachmentNotification({
-              collapsed: false,
-              color: '#e10000',
-              title: {
-                value: 'Failed to Set Servers!',
-                link: 'https://app.plex.tv/desktop#!/account',
-              },
-              text: 'Failed to parse return from API.',
-            }, read, modify, context.getSender(), context.getRoom());
-          }
-        }
-      }
-    }
-  }
-
   private async processServersCommand(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp, persis: IPersistence): Promise<void> {
     const persistence = new AppPersistence(persis, read.getPersistenceReader());
     const servers = await persistence.getUserServers(context.getSender());
@@ -267,9 +243,7 @@ export class PlexCommand implements ISlashCommand {
         const serversList = JSON.parse(servers);
         if (serversList && Array.isArray(serversList)) {
           const userThumbUrl = await persistence.getUserThumb(context.getSender());
-          serversList.forEach(async (server) => {
-            await sendServerDetailsNotification(server, userThumbUrl, read, modify, context.getSender(), context.getRoom());
-          });
+          await sendServersDetailsNotification(serversList, userThumbUrl, read, modify, context.getSender(), context.getRoom());
         }
       } catch (e) {
         await sendAttachmentNotification({

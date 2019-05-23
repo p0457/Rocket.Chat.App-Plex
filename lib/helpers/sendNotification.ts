5cc045892f2355c763e5a35cc82a0675b7a75c45
Thomas Gardner <thomasgardner@orthofix.com>
2019-05-22 19:23:08 -0500

    Added icon and alias

--- lib/helpers/sendNotification.ts
+++ lib/helpers/sendNotification.ts
@@ -3,6 +3,8 @@ import { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
 import { IUser } from '@rocket.chat/apps-engine/definition/users';
 
 export async function sendNotification(text: string, read: IRead, modify: IModify, user: IUser, room: IRoom): Promise<void> {
+    const icon = await read.getEnvironmentReader().getSettings().getValueById('plex_icon');
+    const username = await read.getEnvironmentReader().getSettings().getValueById('plex_name');
     const sender = await read.getUserReader().getById('rocket.cat');
 
     modify.getNotifier().notifyUser(user, modify.getCreator().startMessage({
@@ -10,5 +12,7 @@ export async function sendNotification(text: string, read: IRead, modify: IModif
         room,
         text,
         groupable: false,
+        alias: username,
+        avatarUrl: icon,
     }).getMessage());
 }

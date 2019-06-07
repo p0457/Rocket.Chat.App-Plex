3d57773cc84a6f3ad7d5ad6d319681840a796655
Thomas Gardner <thomasgardner@orthofix.com>
2019-06-07 17:58:35 -0500

    Minor fix

--- commands/PlexPlaybackCommand.ts
+++ commands/PlexPlaybackCommand.ts
@@ -1,11 +1,11 @@
 import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { MessageActionType, MessageProcessingType } from '@rocket.chat/apps-engine/definition/messages';
 import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashcommands';
 import defaultHeaders from '../lib/helpers/defaultHeaders';
 import * as msgHelper from '../lib/helpers/messageHelper';
 import * as request from '../lib/helpers/request';
 import { AppPersistence } from '../lib/persistence';
 import { PlexApp } from '../PlexApp';
-import { MessageActionType, MessageProcessingType } from '@rocket.chat/apps-engine/definition/messages';
 
 export class PlexPlaybackCommand implements ISlashCommand {
   public command = 'plex-playback';

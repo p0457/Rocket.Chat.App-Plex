fe70ed9419571ec918ca689cb9e0621a8970ead8
Thomas Gardner <thomasgardner@orthofix.com>
2019-06-24 14:13:33 -0500

    Fix TS warning

--- commands/PlexScanCommand.ts
+++ commands/PlexScanCommand.ts
@@ -1,5 +1,5 @@
 import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
-import { ISlashCommand, SlashCommandContext, ISlashCommandPreview, ISlashCommandPreviewItem, SlashCommandPreviewItemType } from '@rocket.chat/apps-engine/definition/slashcommands';
+import { ISlashCommand, ISlashCommandPreview, ISlashCommandPreviewItem, SlashCommandContext, SlashCommandPreviewItemType } from '@rocket.chat/apps-engine/definition/slashcommands';
 import defaultHeaders from '../lib/helpers/defaultHeaders';
 import * as msgHelper from '../lib/helpers/messageHelper';
 import * as request from '../lib/helpers/request';
